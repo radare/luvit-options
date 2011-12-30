e1dc20c245940209b114c65bf2c00704ee2dd741
radare <pancake@nopcode.org>
2011-12-30 04:33:23 +0100

    update package.lua and fix help in test

--- package.lua
+++ package.lua
@@ -1,8 +1,7 @@
 return {
-  name = "getopt",
-  version = "0.2",
-  description = "easy to use getopt library",
-  author = "pancake <pancake@nopcode.org>",
-  dependencies = {
-  },
+	name = "options",
+	version = "0.3",
+	description = "easy to use getopt library",
+	author = "pancake <pancake@nopcode.org>",
+	dependencies = { }
 }
